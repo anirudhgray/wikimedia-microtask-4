029c1b5eb9060c89fb0ff81bcbf745e69231e5ae
anirudh <anirudh04mishra@gmail.com>
2022-03-30 20:41:54 +0530

    Add CORS.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from utils import validate_url
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class UrlModel(BaseModel):
     url: str
-
-
-app = FastAPI()
 
 
 @app.post("/", response_model=UrlModel)
